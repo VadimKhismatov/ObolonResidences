b75e7b501c7c5230074a4fb14316bd1940a58701
Vadim Khismatov <vadimkhismatov@outlook.com>
2017-01-18 17:48:10 +0200

    add form for sign up pop up

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@
 //=include lib/jquery.min.js
 //=include lib/slick.min.js
 //=include lib/svgxuse.min.js
+//=include lib/jquery.magnific-popup.js
 
 sayHello();
 
@@ -23,14 +24,14 @@ function initMap() {
     }
 
     places.push(new Place("Obolon Residence", 50.513291, 30.499907, "../img/residence.png"));
-    places.push(new Place("Privat Bank", 50.513867,  30.496080, "../img/banks.png"));
-    places.push(new Place("Metro", 50.513082,  30.497866, "../img/metro.png"));
-    places.push(new Place("Dentist", 50.513780,  30.495270, "../img/dentist.png"));
-    places.push(new Place("Pharmacy", 50.512995,  30.503163, "../img/drugstore.png"));
-    places.push(new Place("Pharmacy", 50.512964,  30.502313, "../img/drugstore.png"));
-    places.push(new Place("Pharmacy", 50.512781,  30.501302, "../img/drugstore.png"));
-    places.push(new Place("Cafe", 50.513468,  30.497356, "../img/cafe.png"));
-    places.push(new Place("Cafe", 50.513475,  30.497976, "../img/cafe.png"));
+    places.push(new Place("Privat Bank", 50.513867, 30.496080, "../img/banks.png"));
+    places.push(new Place("Metro", 50.513082, 30.497866, "../img/metro.png"));
+    places.push(new Place("Dentist", 50.513780, 30.495270, "../img/dentist.png"));
+    places.push(new Place("Pharmacy", 50.512995, 30.503163, "../img/drugstore.png"));
+    places.push(new Place("Pharmacy", 50.512964, 30.502313, "../img/drugstore.png"));
+    places.push(new Place("Pharmacy", 50.512781, 30.501302, "../img/drugstore.png"));
+    places.push(new Place("Cafe", 50.513468, 30.497356, "../img/cafe.png"));
+    places.push(new Place("Cafe", 50.513475, 30.497976, "../img/cafe.png"));
 
     var mapOptions = {
         center: new google.maps.LatLng(50.513291, 30.499907),
@@ -53,3 +54,22 @@ function initMap() {
 
 
 }
+$(document).ready(function () {
+    $('.contact_appointment__link').magnificPopup({
+        type: 'inline',
+        modal: true
+    });
+});
+
+$(document).on('click', '.form--sign_up_close', function (e) {
+    e.preventDefault();
+    $.magnificPopup.close();
+});
+
+console.log($(".benefits_block").offset());
+
+$(window).scroll(function(){
+    console.log($(this).scrollTop());
+});
+
+
