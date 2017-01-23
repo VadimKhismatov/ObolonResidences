e8dca2031a8625ca92bff7f79fcfb7e8b04bec55
Vadim Khismatov <vadimkhismatov@outlook.com>
2017-01-23 12:30:54 +0200

    fixed path for img, fonts, icons

--- src/js/app.js
+++ src/js/app.js
@@ -36,15 +36,15 @@ function initMap() {
         this.icon = icon;  // иконка
     }
 
-    places.push(new Place("Obolon Residence", 50.513291, 30.499907, "../../img/residence.png"));
-    places.push(new Place("Privat Bank", 50.513867, 30.496080, "../../img/banks.png"));
-    places.push(new Place("Metro", 50.513082, 30.497866, "../../img/metro.png"));
-    places.push(new Place("Dentist", 50.513780, 30.495270, "../../img/dentist.png"));
-    places.push(new Place("Pharmacy", 50.512995, 30.503163, "../../img/drugstore.png"));
-    places.push(new Place("Pharmacy", 50.512964, 30.502313, "../../img/drugstore.png"));
-    places.push(new Place("Pharmacy", 50.512781, 30.501302, "../../img/drugstore.png"));
-    places.push(new Place("Cafe", 50.513468, 30.497356, "../../img/cafe.png"));
-    places.push(new Place("Cafe", 50.513475, 30.497976, "../../img/cafe.png"));
+    places.push(new Place("Obolon Residence", 50.513291, 30.499907, "img/residence.png"));
+    places.push(new Place("Privat Bank", 50.513867, 30.496080, "img/banks.png"));
+    places.push(new Place("Metro", 50.513082, 30.497866, "img/metro.png"));
+    places.push(new Place("Dentist", 50.513780, 30.495270, "img/dentist.png"));
+    places.push(new Place("Pharmacy", 50.512995, 30.503163, "img/drugstore.png"));
+    places.push(new Place("Pharmacy", 50.512964, 30.502313, "img/drugstore.png"));
+    places.push(new Place("Pharmacy", 50.512781, 30.501302, "img/drugstore.png"));
+    places.push(new Place("Cafe", 50.513468, 30.497356, "img/cafe.png"));
+    places.push(new Place("Cafe", 50.513475, 30.497976, "img/cafe.png"));
 
     var mapOptions = {
         center: new google.maps.LatLng(50.513291, 30.499907),
